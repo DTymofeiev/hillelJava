4721275caada59b96f2d6f528d1a8b3cac3eaf8c
DTymofeiev <fm.ru68@gmail.com>
2020-05-01 14:56:58 +0300

    calculator

--- src/LESSON1.java
+++ src/LESSON1.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class LESSON1 {
     public static void main(String[] args) {
-        System.out.println("Hello. World");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.println("Please input second number: ");
+        double num2 = scanner.nextDouble();
+        System.out.println("Select an action");
+        double result;
+        char action;
+        action = scanner.next().charAt(0);
+        switch (action) {
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> result = num1 / num2;
+            default -> {
+                System.out.println("Error.Try again .");
+                return;
+            }
+        }
+        System.out.print(result);
+
     }
+
 }
