0b38e6f045102fb34c2200678960db850a6795eb
DTymofeiev <fm.ru68@gmail.com>
2020-07-06 10:29:44 +0300

    LinkedList and JUnit

---
+++ src/lesson_10/LinkedList.java
@@ -0,0 +1,196 @@
+package lesson_10;
+import java.util.Collection;
+import java.util.Iterator;
+
+
+public class LinkedList implements Collection {
+
+    private Node first;
+
+    @Override
+    public int size() {
+        int size = 0;
+        Node currentElement = first;
+        while (currentElement != null) {
+            currentElement = currentElement.getNext();
+            size++;
+        }
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) != -1;
+    }
+
+    private int indexOf(Object o) {
+        int index = 0;
+        Node currentElement = first;
+        while (currentElement != null) {
+            if (o.equals(currentElement.getData())) {
+                return index;
+            }
+            currentElement = currentElement.getNext();
+            index++;
+        }
+        return -1;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+            Node currentElement = first;
+            @Override
+            public boolean hasNext() {
+                return currentElement != null;
+            }
+
+            @Override
+            public Object next() {
+                Object data = currentElement.getData();
+                currentElement = currentElement.getNext();
+                return data;
+            }
+        };
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] result = new Object[size()];
+        int index = 0;
+        Node currentElement = first;
+        while (currentElement != null) {
+            result[index++] = currentElement.getData();
+            currentElement = currentElement.getNext();
+        }
+        return result;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(Object o) {
+        Node last = getLast();
+        Node addedNode = new Node(o, null);
+        if (last == null) {
+            first = addedNode;
+        } else {
+            last.setNext(addedNode);
+        }
+        return true;
+    }
+
+    private Node getLast() {
+        if (first == null) {
+            return null;
+        }
+        Node currentElement = first;
+        while (currentElement.getNext() != null) {
+            currentElement = currentElement.getNext();
+        }
+        return currentElement;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        Node nodeToRemove = findFirstNode(o);
+        if (nodeToRemove == null) {
+            return false;
+        }
+
+        Node previousNode = findPreviousNode(nodeToRemove);
+        if (previousNode == null) {
+            first = nodeToRemove.getNext();
+        } else {
+            previousNode.setNext(nodeToRemove.getNext());
+        }
+        return true;
+    }
+
+    private Node findPreviousNode(Node nodeToRemove) {
+        if (first.equals(nodeToRemove)) {
+            return null;
+        }
+
+        Node currentNode = first;
+        if (currentNode == null) {
+            return null;
+        }
+        while (currentNode.getNext() != null
+                && !currentNode.getNext().equals(nodeToRemove)) {
+            currentNode = currentNode.getNext();
+        }
+        return currentNode;
+    }
+
+    private Node findFirstNode(Object o) {
+        Node currentNode = first;
+        if (currentNode == null) {
+            return null;
+        }
+        while (currentNode != null && !currentNode.getData().equals(o)) {
+            currentNode = currentNode.getNext();
+        }
+        return currentNode;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        Object[] arr = c.toArray();
+        for (Object o : arr) {
+            add(o);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        Object[] arr = c.toArray();
+        for (Object o : arr) {
+            if (!remove(o)) throw new RuntimeException(
+                    "Object " + o + " can't be removed");
+        }
+        return true;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        first = null;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        for (Object i : list) {
+            System.out.println(i);
+        }
+
+//        Iterator it = list.iterator();
+//        while (it.hasNext()) {
+//            System.out.println(it.next());
+//        }
+    }
+}
---
+++ src/lesson_10/LinkedListTest.java
@@ -0,0 +1,106 @@
+package lesson_10;
+import org.junit.*;
+import org.junit.Test;
+
+
+    public class LinkedListTest {
+
+        LinkedList linkedList;
+
+        @Before
+        public void setUp() {
+            linkedList = new LinkedList();
+        }
+
+        @Test
+        public void testSizeEmpty() {
+            int size = linkedList.size();
+            Assert.assertEquals("Expected size empty", 0, size);
+        }
+
+        @Test
+        public void testSizeNotEmpty() {
+            Integer first = 1;
+            Integer second = 2;
+
+            linkedList.add(first);
+            linkedList.add(second);
+
+            Assert.assertEquals(2, linkedList.size());
+        }
+
+        @Test
+        public void testRemove() {
+            Integer first = 1;
+            Integer second = 2;
+
+            linkedList.add(first);
+            linkedList.add(second);
+            boolean result = linkedList.remove(0);
+
+            Assert.assertEquals(2, linkedList.size());
+            Assert.assertFalse(result);
+
+            result = linkedList.remove(1);
+            Assert.assertTrue(result);
+            Assert.assertEquals(1, linkedList.size());
+
+            result = linkedList.remove(2);
+            Assert.assertTrue(result);
+            Assert.assertEquals(0, linkedList.size());
+        }
+
+        @Test
+        public void testContains() {
+            Integer first = 1;
+            Integer second = 2;
+
+            linkedList.add(first);
+            linkedList.add(second);
+
+            boolean result = linkedList.contains(1);
+            Assert.assertTrue("Expected contains() returns true", result);
+
+            result = linkedList.contains(10);
+            Assert.assertFalse("Expected contains() returns false", result);
+        }
+
+        @Test
+        public void testToArray() {
+            Integer first = 1;
+            Integer second = 2;
+
+            linkedList.add(first);
+            linkedList.add(second);
+
+            Assert.assertArrayEquals(new Object[]{1, 2}, linkedList.toArray());
+        }
+
+        @Test
+        public void testAddAll() {
+            linkedList.add(1);
+            linkedList.add(2);
+
+            LinkedList linkedList2 = new LinkedList();
+            linkedList2.add(3);
+            linkedList2.add(4);
+
+            linkedList.addAll(linkedList2);
+
+            Assert.assertEquals(4, linkedList.size());
+        }
+
+        @Test
+        public void testRemoveAll() {
+            linkedList.add(1);
+            linkedList.add(2);
+
+            LinkedList linkedList2 = new LinkedList();
+            linkedList2.add(1);
+
+            linkedList.removeAll(linkedList2);
+
+            Assert.assertEquals(1, linkedList.size());
+        }
+
+    }
