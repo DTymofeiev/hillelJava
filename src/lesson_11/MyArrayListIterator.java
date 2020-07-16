aadbfb61a16f46babef06d02711fb2866c1bf1a0
DTymofeiev <fm.ru68@gmail.com>
2020-07-16 14:28:52 +0300

    ...

---
+++ src/lesson_11/MyArrayListIterator.java
@@ -0,0 +1,25 @@
+package lesson_11;
+
+import java.util.Iterator;
+
+public class MyArrayListIterator<T> implements Iterator<T> {
+
+    private int index = 0;
+    private Object[] objects;
+
+    public MyArrayListIterator(Object[] objects) {
+        this.objects = objects;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return index < objects.length;
+    }
+
+    @Override
+    public T next() {
+        Object result = objects[index];
+        index++;
+        return (T) result;
+    }
+}
